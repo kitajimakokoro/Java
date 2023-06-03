2fc9bc093a3ab809efb9a964cadd90e017d0048a
kitajimakokoro <122000079+kitajimakokoro@users.noreply.github.com>
2023-06-03 05:39:07 +0000

    演算子

---
+++ Practice/seca2-5/ScoreArray.java
@@ -0,0 +1,12 @@
+class ScoreArray {
+  public static void main(String[] args) {
+    int[] date = { 90, 80, 100, 90 };
+
+    System.out.println("1人目は" + date[0] + "点");
+    System.out.println("2人目は" + date[1] + "点");
+    System.out.println("3人目は" + date[2] + "点");
+    System.out.println("4人目は" + date[3] + "点");
+
+    System.out.println("date.length：" + date.length);
+  }
+}
