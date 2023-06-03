22f3debf841abb598dd0750e401220d53e76e13d
kitajimakokoro <122000079+kitajimakokoro@users.noreply.github.com>
2023-06-03 07:08:14 +0000

    分岐処理

---
+++ Training1/Branch3.java
@@ -0,0 +1,19 @@
+class Branch3 {
+  public static void main(String[] args) {
+    int num = Integer.parseInt(args[0]);
+
+    switch( num ){
+      case 1:
+        System.out.println("入園料金：8,400円");
+        break;
+      case 2:
+        System.out.println("入園料金：7,000円");
+        break;
+      case 3:
+        System.out.println("入園料金：5,000円");
+        break;
+      default: //それ以外の値（123以外）の場合
+        System.out.println("１：一般，２：中・高校生，３：小学生・幼児");
+    }
+  }
+}
